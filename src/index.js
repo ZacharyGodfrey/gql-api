024a742e0c4e5a8b606f576f6fee304d36c056e0
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-01-30 10:35:21 -0500

    Disable gql UI on production

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const gqlPlugin = require('./graphql/plugin');
 const server = require('./server');
 
 const port = process.env.PORT || 8080;
+const isProduction = process.env.NODE_ENV === 'production';
 const context = {
     db: {
         name: process.env.DB_NAME || 'db_name'
     }
 };
 
-server.use('/graphql', gqlPlugin(context, { enableUI: true }));
+server.use('/graphql', gqlPlugin(context, { enableUI: !isProduction }));
 
 server.use((_, res) => res.status(404).end());
 
