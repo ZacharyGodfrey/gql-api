adf6c5fe077b97725848e27bebe672eb85b99946
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-01 10:27:17 -0500

    Log requests as JSON

--- src/helpers/log-request.js
+++ src/helpers/log-request.js
@@ -1,18 +1,12 @@
 module.exports = (resolverName, requestContext, args, error, result) => {
-    const displayError = !error ? null : {
-        message: error.message,
-        stack: error.stack.split('\n').slice(1)
-    };
-
-    console.log('==========');
-    console.log(` Resolver: ${resolverName}`);
-    console.log('----------');
-    console.log(`  Context: ${JSON.stringify(requestContext, null, 2)}`);
-    console.log('----------');
-    console.log(`     Args: ${JSON.stringify(args, null, 2)}`);
-    console.log('----------');
-    console.log(`    Error: ${JSON.stringify(displayError, null, 2)}`);
-    console.log('----------');
-    console.log(`   Result: ${JSON.stringify(result, null, 2)}`);
-    console.log('==========');
+    console.log(JSON.stringify({
+        resolverName,
+        requestContext,
+        args,
+        error: !error ? null : {
+            message: error.message,
+            stack: error.stack.split('\n').slice(1)
+        },
+        result
+    }, null, 2));
 };
