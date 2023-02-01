5fa4f7e206c6cdaea71f12fd93ff453a60a514ea
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-01 10:24:23 -0500

    Formatting

--- src/server.js
+++ src/server.js
@@ -8,12 +8,12 @@ server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 
 server.use(({ method }, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST');
-  res.header('x-powered-by', '');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST');
+    res.header('x-powered-by', '');
 
-  method === 'OPTIONS' ? res.status(200).end() : next();
+    method === 'OPTIONS' ? res.status(200).end() : next();
 });
 
 module.exports = server;
